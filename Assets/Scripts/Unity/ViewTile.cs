4438105fb9e00caecf5e2b2d7dd3f37150926006
Aggasti <oliver.lazarevski@gmail.com>
2018-01-27 16:28:20 +0100

    2d World Map Generated

---
+++ Assets/Scripts/Unity/ViewTile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewTile : MonoBehaviour
+{
+
+    public Vector2 worldPosition;
+
+    public SpriteRenderer holeSpriteRenderer;
+    public SpriteRenderer mountainSpriteRenderer;
+    public SpriteRenderer belowCrystalSpriteRenderer;
+    public SpriteRenderer aboveCrystalSpriteRenderer;
+
+    public GameObject TowerGameObject;
+
+    private SpriteRenderer plainSpriteRenderer;
+
+    void Awake()
+    {
+        plainSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangePlainSprite(Sprite sprite)
+    {
+        plainSpriteRenderer.sprite = sprite;
+        aboveCrystalSpriteRenderer.gameObject.SetActive(true);
+    }
+
+    public void ChangeHoleSprite(Sprite sprite)
+    {
+        holeSpriteRenderer.gameObject.SetActive(true);
+        holeSpriteRenderer.sprite = sprite;
+    }
+
+    public void RemoveHoleSprite()
+    {
+        holeSpriteRenderer.gameObject.SetActive(false);
+    }
+
+    public void ChangeMountainSprite(Sprite sprite)
+    {
+        aboveCrystalSpriteRenderer.gameObject.SetActive(false);
+        mountainSpriteRenderer.gameObject.SetActive(true);
+        mountainSpriteRenderer.sprite = sprite;
+        DestroyTower();
+    }
+
+    public void RemoveMountain()
+    {
+        aboveCrystalSpriteRenderer.gameObject.SetActive(true);
+        mountainSpriteRenderer.gameObject.SetActive(false);
+    }
+
+    public void SetResourceType(Sprite spriteAbove, Sprite spriteBelow)
+    {
+        aboveCrystalSpriteRenderer.sprite = spriteAbove;
+        belowCrystalSpriteRenderer.sprite = spriteBelow;
+    }
+
+    public void ActivateTower()
+    {
+        TowerGameObject.SetActive(true);
+    }
+
+    public void DestroyTower()
+    {
+        TowerGameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Unity/WorldCreator.cs
+++ Assets/Scripts/Unity/WorldCreator.cs
@@ -5,56 +5,133 @@ using UnityEngine;
 
 public class WorldCreator : MonoBehaviour
 {
-    public GameObject Plain, Mine, Mountain;
+    public Sprite[] PlainSprites;
+    public Sprite[] MountainSprites;
+    public Sprite[] HoleSprites;
+    public Sprite redBelow, redAbove, greenBelow, greenAbove, blueBelow, blueAbove;
+    //public Sprite towerSprite;
 
-    private World World;
+    public ViewTile[] ViewTilesPlayerOne;
+    public ViewTile[] ViewTilesPlayerTwo;
+
+    private ViewTile[,] _viewTilesMultiArrayPlayerOne = new ViewTile[6, 5];
+    private ViewTile[,] _viewTilesMultiArrayPlayerTwo = new ViewTile[6, 5];
+
+    [HideInInspector]
+    public World World;
 
     void Start()
     {
         World = new World();
 
+        FillViewTilesMultiArray();
+
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
         for (int i = 0; i < World.Length; i++)
         {
             for (int j = 0; j < World.Width; j++)
             {
-                GameObject tilePlain;
-                if (World.PlayerOne.Tiles[i, j].TileType == TileType.Mountain)
-                {
-                    tilePlain = Instantiate(Plain, new Vector3(i, 0, j), Quaternion.identity);
-                    GameObject tileMountain = Instantiate(Mountain, new Vector3(i, 0, j), Quaternion.identity);
-                }
-                else if (World.PlayerOne.Tiles[i, j].TileType == TileType.Mine)
-                {
-                    tilePlain = Instantiate(Mine, new Vector3(i, 0, j), Quaternion.identity);
-
-                }
-                else
-                {
-                    tilePlain = Instantiate(Plain, new Vector3(i, 0, j), Quaternion.identity);
-                }
-                tilePlain.name = string.Format("P1[{0}, {1}]", i, j);
+                GenerateMapP1(i, j);
+                GenerateMapP2(i, j);
             }
         }
+    }
 
+    private void GenerateMapP1(int i, int j)
+    {
+        if (World.PlayerOne.Tiles[i, j].ResourceType == ResourceType.Red)
+        {
+            _viewTilesMultiArrayPlayerOne[i, j].SetResourceType(redAbove, redBelow);
+        }
+        else if (World.PlayerOne.Tiles[i, j].ResourceType == ResourceType.Green)
+        {
+            //_viewTilesMultiArrayPlayerOne[i, j].SetResourceType(greenAbove, greenBelow);
+            _viewTilesMultiArrayPlayerOne[i, j].aboveCrystalSpriteRenderer.color = Color.green;
+            _viewTilesMultiArrayPlayerOne[i, j].belowCrystalSpriteRenderer.color = Color.green;
+        }
+        else
+        {
+            //_viewTilesMultiArrayPlayerOne[i, j].SetResourceType(blueAbove, blueBelow);
+            _viewTilesMultiArrayPlayerOne[i, j].aboveCrystalSpriteRenderer.color = Color.blue;
+            _viewTilesMultiArrayPlayerOne[i, j].belowCrystalSpriteRenderer.color = Color.blue;
+        }
+
+        _viewTilesMultiArrayPlayerOne[i, j]
+            .ChangePlainSprite(PlainSprites[UnityEngine.Random.Range(0, PlainSprites.Length)]);
+
+        if (World.PlayerOne.Tiles[i, j].TileType == TileType.Mountain)
+        {
+            _viewTilesMultiArrayPlayerOne[i, j]
+                .ChangeMountainSprite(MountainSprites[UnityEngine.Random.Range(0, MountainSprites.Length)]);
+        }
+        else if (World.PlayerOne.Tiles[i, j].TileType == TileType.Mine)
+        {
+            _viewTilesMultiArrayPlayerOne[i, j]
+                .ChangeHoleSprite(HoleSprites[UnityEngine.Random.Range(0, HoleSprites.Length)]);
+        }
+    }
+    private void GenerateMapP2(int i, int j)
+    {
+        if (World.PlayerTwo.Tiles[i, j].ResourceType == ResourceType.Red)
+        {
+            _viewTilesMultiArrayPlayerTwo[i, j].SetResourceType(redAbove, redBelow);
+        }
+        else if (World.PlayerTwo.Tiles[i, j].ResourceType == ResourceType.Green)
+        {
+            //_viewTilesMultiArrayPlayerTwo[i, j].SetResourceType(greenAbove, greenBelow);
+            _viewTilesMultiArrayPlayerTwo[i, j].aboveCrystalSpriteRenderer.color = Color.green;
+            _viewTilesMultiArrayPlayerTwo[i, j].belowCrystalSpriteRenderer.color = Color.green;
+        }
+        else
+        {
+            //_viewTilesMultiArrayPlayerTwo[i, j].SetResourceType(blueAbove, blueBelow);
+            _viewTilesMultiArrayPlayerTwo[i, j].aboveCrystalSpriteRenderer.color = Color.blue;
+            _viewTilesMultiArrayPlayerTwo[i, j].belowCrystalSpriteRenderer.color = Color.blue;
+        }
+
+        _viewTilesMultiArrayPlayerTwo[i, j]
+            .ChangePlainSprite(PlainSprites[UnityEngine.Random.Range(0, PlainSprites.Length)]);
+
+        if (World.PlayerTwo.Tiles[i, j].TileType == TileType.Mountain)
+        {
+            _viewTilesMultiArrayPlayerTwo[i, j]
+                .ChangeMountainSprite(MountainSprites[UnityEngine.Random.Range(0, MountainSprites.Length)]);
+        }
+        else if (World.PlayerTwo.Tiles[i, j].TileType == TileType.Mine)
+        {
+            _viewTilesMultiArrayPlayerTwo[i, j]
+                .ChangeHoleSprite(HoleSprites[UnityEngine.Random.Range(0, HoleSprites.Length)]);
+        }
+    }
+
+    public void FillViewTilesMultiArray()
+    {
         for (int i = 0; i < World.Length; i++)
         {
-            for (int j = World.Width-1; j >= 0; j--)
+            for (int j = 0; j < World.Width; j++)
             {
-                GameObject tilePlain;
-                if (World.PlayerTwo.Tiles[i, j].TileType == TileType.Mountain)
+                foreach (var viewTile in ViewTilesPlayerOne)
                 {
-                    tilePlain = Instantiate(Plain, new Vector3(i, 0, 12-j), Quaternion.identity);
-                    GameObject tileMountain = Instantiate(Mountain, new Vector3(i, 0, 12 - j), Quaternion.identity);
+                    if (viewTile.worldPosition.x == i && viewTile.worldPosition.y == j)
+                    {
+                        _viewTilesMultiArrayPlayerOne[i, j] = viewTile;
+                        break;
+                    }
+
                 }
-                else if (World.PlayerTwo.Tiles[i, j].TileType == TileType.Mine)
+                foreach (var viewTile in ViewTilesPlayerTwo)
                 {
-                    tilePlain = Instantiate(Mine, new Vector3(i, 0, 12 - j), Quaternion.identity);
+                    if (viewTile.worldPosition.x == i && viewTile.worldPosition.y == j)
+                    {
+                        _viewTilesMultiArrayPlayerTwo[i, j] = viewTile;
+                        break;
+                    }
+
                 }
-                else
-                {
-                    tilePlain = Instantiate(Plain, new Vector3(i, 0, 12 - j), Quaternion.identity);
-                }
-                tilePlain.name = string.Format("P2[{0}, {1}]", i, j);
             }
         }
 
