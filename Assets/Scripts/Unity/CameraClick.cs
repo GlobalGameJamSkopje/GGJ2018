8b11da684d7300c3160c30342531a2723e894790
Aggasti <oliver.lazarevski@gmail.com>
2018-01-27 18:42:42 +0100

    Added 2dRaycast and movement of the playerSprite

---
+++ Assets/Scripts/Unity/CameraClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClick : MonoBehaviour
+{
+
+    public GameObject playerSprite;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                playerSprite.transform.position = hit.collider.gameObject.transform.position;
+
+            }
+        }
+        
+    }
+}
