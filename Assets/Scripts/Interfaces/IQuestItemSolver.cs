ca49101556072779998e0e7c717ac75d6340a4ca
sumarsky <i.dimcevski@gmail.com>
2018-01-28 10:37:39 +0100

    questsolver for all quests

--- Assets/Scripts/Interfaces/IQuestItemSolver.cs
+++ Assets/Scripts/Interfaces/IQuestItemSolver.cs
@@ -1,5 +1,8 @@
 ﻿
+using System.Collections.Generic;
+
 public interface IQuestItemSolver
 {
+    bool CanBeSolved(List<QuestItem> quests, PlayerResources playerResources);
     bool CanBeSolved(QuestItem questItem, PlayerResources playerResources);
 }
--- Assets/Scripts/QuestItemSolver.cs
+++ Assets/Scripts/QuestItemSolver.cs
@@ -1,6 +1,24 @@
 ﻿
+using System.Collections.Generic;
+using System.Linq;
+
 public class QuestItemSolver : IQuestItemSolver
 {
+    public bool CanBeSolved(List<QuestItem> quests, PlayerResources playerResources)
+    {
+        var totalRedRequired = quests.Sum(x => x.RequiredRedResources);
+        var totalGreenRequired = quests.Sum(x => x.RequiredGreenResources);
+        var totalBlueRequired = quests.Sum(x => x.RequiredBlueResources);
+
+        var redCompleted = totalRedRequired <= playerResources.NumberOfRedResources;
+        var greenCompleted = totalGreenRequired <= playerResources.NumberOfGreenResources;
+        var blueCompleted = totalBlueRequired <= playerResources.NumberOfBlueResources;
+
+        return redCompleted 
+               && greenCompleted 
+               && blueCompleted;
+    }
+
     public bool CanBeSolved(QuestItem questItem, PlayerResources playerResources)
     {
         var areRedResourcesMatched = questItem.RequiredRedResources <= playerResources.NumberOfRedResources;
@@ -11,4 +29,5 @@ public class QuestItemSolver : IQuestItemSolver
                && areGreenResourcesMatched
                && areBlueResourcesMatched;
     }
+
 }
