1c3b89801fb3d7e30ed9a758cdf39fc6807098b5
Aggasti <oliver.lazarevski@gmail.com>
2018-01-28 11:44:05 +0100

    basic UI and logic for UI

--- Assets/Scripts/Unity/GameController.cs
+++ Assets/Scripts/Unity/GameController.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject Player1;
     public GameObject Player2;
 
+    public Text RedResourcesText, GreenResourcesText, BlueResourcesText;
+    public Text Quest1Text, Quest2Text, Quest3Text, Quest4Text, Quest5Text;
+
+    public Image FirstSideQuestHoldImage,SecondSideQuestHoldImage;
+    public Text FirstSideQuestText, SecondSideQuestText;
+
     private WorldCreator _worldCreator;
     private World _world;
+    private QuestItemSolver _questSolver;
 
     private ViewTile _p1PositionTile;
     private ViewTile _p2PositionTile;
@@ -19,6 +27,7 @@ public class GameController : MonoBehaviour
     void Awake()
     {
         _world = new World();
+        _questSolver = new QuestItemSolver();
         _worldCreator = FindObjectOfType<WorldCreator>();
     }
 
@@ -51,7 +60,106 @@ public class GameController : MonoBehaviour
         }
     }
 
-    public void DigP1(ViewTile tile)
+    public void NextTurn()
+    {
+        if (_currentPlayer == PlayerIndex.P1) Player2Turn();
+        else Player1Turn();
+    }
+
+    public void Build()
+    {
+        if (_currentPlayer == PlayerIndex.P1)
+            BuildP1(_p1PositionTile);
+        else
+            BuildP2(_p2PositionTile);
+    }
+
+    public void Dig()
+    {
+        if (_currentPlayer == PlayerIndex.P1)
+            DigP1(_p1PositionTile);
+        else
+            DigP2(_p2PositionTile);
+    }
+
+    public void HoldFirstSideQuest()
+    {
+        if (_currentPlayer == PlayerIndex.P1)
+        {
+            _world.P1SideQuest.ToggleHoldOnQuest(_world.P1SideQuest.SideQuests[0]);
+            //TODO: change sprite
+        }
+        else
+        {
+            _world.P2SideQuest.ToggleHoldOnQuest(_world.P2SideQuest.SideQuests[0]);
+            //TODO: change sprite
+        }
+    }
+
+    public void HoldSecondSideQuest()
+    {
+        if (_currentPlayer == PlayerIndex.P1)
+        {
+            _world.P1SideQuest.ToggleHoldOnQuest(_world.P1SideQuest.SideQuests[1]);
+            //TODO:change sprite
+        }
+        else
+        {
+            _world.P2SideQuest.ToggleHoldOnQuest(_world.P2SideQuest.SideQuests[1]);
+            //TODO: change sprite
+        }
+    }
+
+    public void CompleteFirstSideQuest()
+    {
+        if (_currentPlayer == PlayerIndex.P1)
+        {
+            var quest = _world.P1SideQuest.SideQuests[0];
+            if (_questSolver.CanBeSolved(quest, _world.P1Resources))
+            {          
+                _world.P1SideQuest.CompleteQuest(quest);
+                _world.P1Action.GetSideQuestReward(quest);
+                //TODO: refresh actions UI
+            }
+        }
+        else
+        {
+            var quest = _world.P2SideQuest.SideQuests[0];
+            if (_questSolver.CanBeSolved(quest, _world.P1Resources))
+            {
+                _world.P2SideQuest.CompleteQuest(quest);
+                _world.P2Action.GetSideQuestReward(quest);
+                //TODO: refresh actions UI
+            }
+        }
+    }
+
+    public void CompleteSecondSideQuest()
+    {
+        if (_currentPlayer == PlayerIndex.P1)
+        {
+            var quest = _world.P1SideQuest.SideQuests[1];
+            if (_questSolver.CanBeSolved(quest, _world.P1Resources))
+            {
+                _world.P1SideQuest.CompleteQuest(quest);
+                _world.P1Action.GetSideQuestReward(quest);
+                //TODO: refresh actions UI
+            }
+        }
+        else
+        {
+            var quest = _world.P2SideQuest.SideQuests[1];
+            if (_questSolver.CanBeSolved(quest, _world.P1Resources))
+            {
+                _world.P2SideQuest.CompleteQuest(quest);
+                _world.P2Action.GetSideQuestReward(quest);
+                //TODO: refresh actions UI
+            }
+        }
+    }
+
+
+    private void DigP1(ViewTile tile)
     {
         if (!_world.P1Action.CanUseAction(PlayerActionType.Dig)) return;
 
@@ -94,7 +202,7 @@ public class GameController : MonoBehaviour
         }
     }
 
-    public void DigP2(ViewTile tile)
+    private void DigP2(ViewTile tile)
     {
         if (!_world.P2Action.CanUseAction(PlayerActionType.Dig)) return;
 
@@ -138,11 +246,11 @@ public class GameController : MonoBehaviour
         }
     }
 
-    public void BuildP1(ViewTile tile)
+    private void BuildP1(ViewTile tile)
     {
-        if (!_world.P1Action.CanUseAction(PlayerActionType.Build)) return;
-
         var tileP1 = _world.P1Plain.Tiles[(int)tile.worldPosition.x, (int)tile.worldPosition.y];
+
+        if (!_world.P1Action.CanUseAction(PlayerActionType.Build) || tileP1.TowerActive) return;
 
         switch (tileP1.TileType)
         {
@@ -164,13 +272,15 @@ public class GameController : MonoBehaviour
             case TileType.Mountain:
                 break;
         }
+
+        RefreshUIP1();
     }
 
-    public void BuildP2(ViewTile tile)
+    private void BuildP2(ViewTile tile)
     {
-        if (!_world.P2Action.CanUseAction(PlayerActionType.Build)) return;
-
         var tileP2 = _world.P2Plain.Tiles[(int)tile.worldPosition.x, (int)tile.worldPosition.y];
+
+        if (!_world.P2Action.CanUseAction(PlayerActionType.Build) || tileP2.TowerActive) return;
 
         switch (tileP2.TileType)
         {
@@ -191,9 +301,11 @@ public class GameController : MonoBehaviour
             case TileType.Mountain:
                 break;
         }
+
+        RefreshUIP2();
     }
 
-    public void MoveP1(ViewTile tile)
+    private void MoveP1(ViewTile tile)
     {
         if (!_world.P1Action.CanUseAction(PlayerActionType.Move)) return;
 
@@ -208,7 +320,7 @@ public class GameController : MonoBehaviour
         }
     }
 
-    public void MoveP2(ViewTile tile)
+    private void MoveP2(ViewTile tile)
     {
         if (!_world.P2Action.CanUseAction(PlayerActionType.Move)) return;
 
@@ -223,24 +335,64 @@ public class GameController : MonoBehaviour
         }
     }
 
-    public void Player1Turn()
+    private void Player1Turn()
     {
+        _world.P1SideQuest.RefillSideQuests();
+        RefreshUIP1();
         Camera.main.transform.position = new Vector3(0, 0, -10);
         _world.P1Action.NewTurn();
         _currentPlayer = PlayerIndex.P1;
     }
 
-    public void Player2Turn()
+    private void Player2Turn()
     {
+        _world.P2SideQuest.RefillSideQuests();
+        RefreshUIP2();
         Camera.main.transform.position = new Vector3(20, 0, -10);
         _world.P2Action.NewTurn();
         _currentPlayer = PlayerIndex.P2;
 
     }
 
-    public void ChangeTurn()
+    private void RefreshUIP1()
     {
-        if (_currentPlayer == PlayerIndex.P1) Player2Turn();
-        else Player1Turn();
+        RedResourcesText.text = "x " + _world.P1Resources.NumberOfRedResources;
+        GreenResourcesText.text = "x " + _world.P1Resources.NumberOfGreenResources;
+        BlueResourcesText.text = "x " + _world.P1Resources.NumberOfBlueResources;
+
+        FirstSideQuestText.text = _world.P1SideQuest.SideQuests[0].ToString();
+        SecondSideQuestText.text = _world.P1SideQuest.SideQuests[1].ToString();
+
+
+        Quest1Text.text = "red : " + _world.Quests[0].RequiredRedResources.ToString() + " : " + _world.P1Resources.NumberOfRedResources + "\n"
+            + "green : " + _world.Quests[0].RequiredGreenResources.ToString() + " : " + _world.P1Resources.NumberOfGreenResources + "\n"
+            + "blue : " + _world.Quests[0].RequiredBlueResources.ToString() + " : " + _world.P1Resources.NumberOfBlueResources;
+
+        Quest2Text.text = "red : " + _world.Quests[1].RequiredRedResources.ToString() + " : " + _world.P1Resources.NumberOfRedResources + "\n"
+                          + "green : " + _world.Quests[1].RequiredGreenResources.ToString() + " : " + _world.P1Resources.NumberOfGreenResources + "\n"
+                          + "blue : " + _world.Quests[1].RequiredBlueResources.ToString() + " : " + _world.P1Resources.NumberOfBlueResources;
+
+        Quest3Text.text = "red : " + _world.Quests[2].RequiredRedResources.ToString() + " : " + _world.P1Resources.NumberOfRedResources + "\n"
+                          + "green : " + _world.Quests[2].RequiredGreenResources.ToString() + " : " + _world.P1Resources.NumberOfGreenResources + "\n"
+                          + "blue : " + _world.Quests[2].RequiredBlueResources.ToString() + " : " + _world.P1Resources.NumberOfBlueResources;
+
+        Quest4Text.text = "red : " + _world.Quests[3].RequiredRedResources.ToString() + " : " + _world.P1Resources.NumberOfRedResources + "\n"
+                          + "green : " + _world.Quests[3].RequiredGreenResources.ToString() + " : " + _world.P1Resources.NumberOfGreenResources + "\n"
+                          + "blue : " + _world.Quests[3].RequiredBlueResources.ToString() + " : " + _world.P1Resources.NumberOfBlueResources;
+
+        Quest5Text.text = "red : " + _world.Quests[4].RequiredRedResources.ToString() + " : " + _world.P1Resources.NumberOfRedResources + "\n"
+                          + "green : " + _world.Quests[4].RequiredGreenResources.ToString() + " : " + _world.P1Resources.NumberOfGreenResources + "\n"
+                          + "blue : " + _world.Quests[4].RequiredBlueResources.ToString() + " : " + _world.P1Resources.NumberOfBlueResources;
     }
+
+    private void RefreshUIP2()
+    {
+        RedResourcesText.text = "x " + _world.P2Resources.NumberOfRedResources;
+        GreenResourcesText.text = "x " + _world.P2Resources.NumberOfGreenResources;
+        BlueResourcesText.text = "x " + _world.P2Resources.NumberOfBlueResources;
+
+        FirstSideQuestText.text = _world.P2SideQuest.SideQuests[0].ToString();
+        SecondSideQuestText.text = _world.P2SideQuest.SideQuests[1].ToString();
+    }
+
 }
